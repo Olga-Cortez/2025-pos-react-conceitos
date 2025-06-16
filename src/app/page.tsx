713b97547c7a192722ef35b7194d47cabdf132d3
Olga Cortez <souza.cortez.013@gmail.com>
2025-06-16 13:20:37 +0000

    Compartilhando Dados Entre Componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,40 +4,22 @@ import React, { Component, useState, useEffect } from 'react';
 
 
 
-interface SaudacaoProps {
-    nome: string;
-  }
-  
-function Titulo(props) {
-  return <h1 className='titulo'>React - conceitos básicos</h1>;
-}
+function Contador() {
+  const [contagem, setContagem] = useState(0);
 
-const Saudacao: React.FC<SaudacaoProps> = ({nome}) => {
-  return <h2>Olá, {nome}!</h2>;
-}
-
-class SaudacaoComClass extends React.Component<SaudacaoProps> {
-  render() {
-    const { nome } = this.props;
-    return <h2>Olá, {nome}!</h2>;
-  }
+  return (
+    <div>
+      <p>Você clicou {contagem} vezes</p>
+      <button onClick={() => setContagem(contagem + 1)}>Clique aqui</button>
+    </div>
+  );
 }
 
 const Home = () => {
-  const [dados, setDados] = useState([]);
-
-  useEffect(() => {
-    fetch('/api/dados')
-      .then(response => response.json())
-      .then(data => setDados(data));
-  }, []);
-
   return (
-    <ul>
-      {dados.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
+    <div>
+      <Contador />
+    </div>
   );
 }
 
