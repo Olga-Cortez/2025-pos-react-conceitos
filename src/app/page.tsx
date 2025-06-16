57d4de80b8a6f351d60979ade2bb4d53abe24835
Olga Cortez <souza.cortez.013@gmail.com>
2025-06-16 12:52:31 +0000

    components tipados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
 "use client";
 
+import React, { Component } from 'react';
+
+
+interface SaudacaoProps {
+    nome: string;
+  }
+  
 function Titulo(props) {
   return <h1>React - conceitos básicos</h1>;
 }
 
-const Saudacao = ({ nome }) => {
+const Saudacao: React.FC<SaudacaoProps> = ({nome}) => {
   return <h2>Olá, {nome}!</h2>;
 }
 
+class SaudacaoComClass extends React.Component<SaudacaoProps> {
+  render() {
+    const { nome } = this.props;
+    return <h2>Olá, {nome}!</h2>;
+  }
+}
+
 const Home = () => {
-  return (
-    <div>
+	return (
+		<div>
 			<main>
 				<Titulo />
-        <Saudacao nome="Professor" />
+        <Saudacao nome="Minora" />
         <Saudacao nome="Alunos" />
+        <SaudacaoComClass nome="professor e alunos" />
 			</main>
-    </div>
-  );
+		</div>
+	);
 }
+
+export default Home;
